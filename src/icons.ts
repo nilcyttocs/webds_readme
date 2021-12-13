1762315cdfa66730d648a06e53d14cda9c0f2c9c
nilcyttocs <root@pi-syna>
2021-12-12 22:10:53 -0800

    Update package scope

--- src/icons.ts
+++ src/icons.ts
@@ -3,6 +3,6 @@ import { LabIcon } from '@jupyterlab/ui-components';
 import readmeSvg from '../style/icons/place-localizer-svgrepo-com.svg';
 
 export const readmeIcon = new LabIcon({
-  name: 'webds_readme:documentation',
+  name: 'webds_readme_icon',
   svgstr: readmeSvg
 });
--- src/index.ts
+++ src/index.ts
@@ -10,28 +10,28 @@ import { readmeIcon } from './icons';
 const readmeDoc = 'Synaptics/README';
 
 /**
- * Initialization data for the webds_readme extension.
+ * Initialization data for the @webds/readme extension.
  */
 const plugin: JupyterFrontEndPlugin<void> = {
-  id: 'webds_readme:plugin',
+  id: '@webds/readme:plugin',
   autoStart: true,
   requires: [ILauncher],
   activate: (app: JupyterFrontEnd, launcher: ILauncher) => {
-    console.log('JupyterLab extension webds_readme is activated!');
+    console.log('JupyterLab extension @webds/readme is activated!');
 
     const { commands, shell } = app;
     const command: string = 'webds_readme:open';
     commands.addCommand(command, {
       label: 'README',
       caption: 'README',
-      icon: (args: { [x: string]: any; }) => (args['isLauncher'] ? readmeIcon : undefined),
+      icon: (args: { [x: string]: any }) => (args['isLauncher'] ? readmeIcon : undefined),
       execute: async () => {
         commands.execute('docmanager:open', {
           path: readmeDoc,
           factory: 'Markdown Preview',
         })
         .then((widget) => {
-          widget.id = 'webds_readme';
+          widget.id = 'webds_readme_widget';
           widget.title.closable = true;
           if (!widget.isAttached)
             shell.add(widget, 'main');
