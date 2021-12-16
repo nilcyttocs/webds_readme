107cd67553e02b34aa208b56d799b32cfc14da48
Scott Lin <scott.lin@tw.synaptics.com>
2021-12-16 08:11:42 -0800

    Update location of README symlink

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ import { ILauncher } from '@jupyterlab/launcher';
 
 import { readmeIcon } from './icons';
 
-const readmeDoc = 'Synaptics/README';
+const readmeDoc = 'Synaptics/.links/README';
 
 /**
  * Initialization data for the @webds/readme extension.
