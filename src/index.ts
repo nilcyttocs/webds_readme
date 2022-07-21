3f119825fcb4b8d07ed07d5b14c310125371eb07
Scott Lin <scott.lin@tw.synaptics.com>
2022-07-21 09:33:33 -0700

    Reorganize launcher page

--- src/index.ts
+++ src/index.ts
@@ -1,47 +1,61 @@
 import {
   JupyterFrontEnd,
   JupyterFrontEndPlugin
-} from '@jupyterlab/application';
+} from "@jupyterlab/application";
 
-import { ILauncher } from '@jupyterlab/launcher';
+import { ILauncher } from "@jupyterlab/launcher";
 
-import { readmeIcon } from './icons';
+import { readmeIcon } from "./icons";
 
-const readmeDoc = 'Synaptics/_links/README';
+namespace Attributes {
+  export const command = "webds_readme:open";
+  export const id = "webds_readme_widget";
+  export const label = "README";
+  export const caption = "README";
+  export const category = "DSDK - Application";
+  export const rank = 10;
+}
+
+const readmeDoc = "Synaptics/_links/README";
 
 /**
  * Initialization data for the @webds/readme extension.
  */
 const plugin: JupyterFrontEndPlugin<void> = {
-  id: '@webds/readme:plugin',
+  id: "@webds/readme:plugin",
   autoStart: true,
   requires: [ILauncher],
   activate: (app: JupyterFrontEnd, launcher: ILauncher) => {
-    console.log('JupyterLab extension @webds/readme is activated!');
+    console.log("JupyterLab extension @webds/readme is activated!");
 
     const { commands, shell } = app;
-    const command: string = 'webds_readme:open';
+    const command = Attributes.command;
     commands.addCommand(command, {
-      label: 'README',
-      caption: 'README',
-      icon: (args: { [x: string]: any }) => (args['isLauncher'] ? readmeIcon : undefined),
+      label: Attributes.label,
+      caption: Attributes.caption,
+      icon: (args: { [x: string]: any }) =>
+        args["isLauncher"] ? readmeIcon : undefined,
       execute: async () => {
-        commands.execute('docmanager:open', {
-          path: readmeDoc,
-          factory: 'Markdown Preview',
-        })
-        .then((widget) => {
-          widget.id = 'webds_readme_widget';
-          widget.title.closable = true;
-          if (!widget.isAttached)
-            shell.add(widget, 'main');
-          shell.activateById(widget.id);
-        });
-      },
-
+        commands
+          .execute("docmanager:open", {
+            path: readmeDoc,
+            factory: "Markdown Preview"
+          })
+          .then((widget) => {
+            widget.id = Attributes.id;
+            widget.title.closable = true;
+            if (!widget.isAttached) shell.add(widget, "main");
+            shell.activateById(widget.id);
+          });
+      }
     });
 
-    launcher.add({ command, args: { isLauncher: true }, category: 'WebDS', rank: 0 });
+    launcher.add({
+      command,
+      args: { isLauncher: true },
+      category: Attributes.category,
+      rank: Attributes.rank
+    });
   }
 };
 
