f35fef5813522e39dc41b13e875862582a38260e
Scott Lin <scott.lin@tw.synaptics.com>
2022-10-24 19:44:36 -0700

    Change icon

--- src/icons.ts
+++ src/icons.ts
@@ -1,6 +1,6 @@
 import { LabIcon } from '@jupyterlab/ui-components';
 
-import readmeSvg from '../style/icons/place-localizer-svgrepo-com.svg';
+import readmeSvg from '../style/icons/information-info-svgrepo-com.svg';
 
 export const readmeIcon = new LabIcon({
   name: 'webds_readme_icon',
